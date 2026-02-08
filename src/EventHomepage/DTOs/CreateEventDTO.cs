ab1d36ab92eef3b64b19fa077943b9940e230e00
Jasho1921 <jasharmirzai@msn.com>
2026-02-09 00:23:44 +0100

    Just random comment for myself...

--- src/EventHomepage/DTOs/CreateEventDTO.cs
+++ src/EventHomepage/DTOs/CreateEventDTO.cs
@@ -8,3 +8,11 @@ public record CreateEventDTO(
     string Location,
     int? MaxParticipants
 );
+
+/*
+CreateEventDTO = Input, används för att skicka data TILL servern
+EventDTO = Output, avnänds för att skicka data FRÅN servern och innehåller ALLA fält
+Jag använder dock inte API i min MVC(HomeController) utom jag använder ist DbContext direkt i MVC:n
+
+För den här gången!..
+*/
